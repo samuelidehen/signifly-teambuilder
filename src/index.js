d62265cc9e51a6df115a48fcacaeaacd19800a99
Samuel Idehen <samuelidehenlion@gmail.com>
2020-04-02 20:06:12 -0400

    --minor routing fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+var history = createBrowserHistory();
+
+history.listen(location => {
+  // Use setTimeout to make sure this runs after React Router's own listener
+  setTimeout(() => {
+    // Keep default behavior of restoring scroll position when user:
+    // - clicked back button
+    // - clicked on a link that programmatically calls `history.goBack()`
+    // - manually changed the URL in the address bar (here we might want
+    // to scroll to top, but we can't differentiate it from the others)
+    if (location.action === "POP") {
+      return;
+    }
+    // In all other cases, scroll to top
+    window.scrollTo(0, 0);
+  });
+});
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Router history={history}>
+    <App />
+  </Router>,
   document.getElementById("root")
 );
 
